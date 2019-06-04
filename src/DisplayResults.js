5aebc39cfc8e5bbbce96ebe6a71136580987ab38
Can Rozanes <canrozanes@gmail.com>
2019-06-04 14:38:44 -0400

    components initialized

--- src/DisplayResults.js
+++ src/DisplayResults.js
@@ -1,1 +1,10 @@
 import React, { Component } from 'react';
+
+class DisplayResults extends Component{
+    render(){
+        return (
+            <h1>DisplayResults</h1>
+        )
+    }
+}
+export default DisplayResults;
--- src/Footer.js
+++ src/Footer.js
@@ -1,1 +1,10 @@
 import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return(
+            <h1>Footer</h1>
+        )
+    }
+}
+export default Footer;
