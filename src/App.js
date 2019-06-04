480bce31efb23f225971f9b9944742d5244eda6b
Can Rozanes <canrozanes@gmail.com>
2019-06-04 18:48:40 -0400

    working on props

--- src/App.js
+++ src/App.js
@@ -12,7 +12,14 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      data:null
+      data:[],
+      eventsJSX: "",
+      eventName: "",
+      eventStartTime: "",
+      eventStartDate: "",
+      eventVenue: "",
+      eventImage: "",
+      
     }
   }
   //time input format localStartEndDateTime=2019-06-05T17:00:00,2019-06-05T20:00:00 
@@ -22,23 +29,35 @@ class App extends Component {
       url: `https://app.ticketmaster.com/discovery/v2/events.json?apikey=cpqJuV2A3YqkXOJylkTrDzVGLRKZ5hp5&city=${city}&localStartEndDateTime=${startDate},${endDate}`,
       dataResponse: "jsonp",
     }).then((response) => {
-      console.log(response)
       this.setState({
-        data: response.data._embedded,
+        data: response.data._embedded.events,
       })
-      console.log(this.state.data);
+      console.log(this.state.data)
+      // console.log(this.parseDataToDisplay());
     })
 
   }
 
+  parseDataToDisplay = () =>{
+    let arrayToParse = [...this.state.data];
+    const eventsJSX = arrayToParse.map((eventObject)=>{
+      return <h3>{eventObject.name}</h3>
+    })
+    this.setState({
+      eventsJSX: eventsJSX,
+    })
+  }
 
-  render(){
+  componentDidMount(){
     this.getTicketmasterData("Toronto", "2019-06-05T18:00:00", "2019-06-05T19:00:00")
+  }
+  render(){
     return (
       <div className="App">
         <Header/>
         <SearchForm/>
-        <DisplayResults/>
+        <DisplayResults
+        events={this.state.data}/>
         <EmailForm/>
         <EmailSent/>
         <Footer/>
