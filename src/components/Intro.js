8a7d3379f25332532de469ef8a3d9b6212211244
anniebombanie <anniebombanie@gmail.com>
2019-06-09 22:52:16 -0400

    removed searchForm.scss

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 
 class Intro extends Component {
-    render() {
-        return(
-            <div className="heroIntro">
-                <h1>Personal <span>Date Night</span></h1>
-                <p>Sometimes you just need a break from your partner but you already got a babysitter. Now couples can go to different events in similar locations and at the same time.</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut aliquam obcaecati totam quasi est quia in sapiente animi illo dicta.</p>
-            </div>
-        )
-    }
+  render() {
+    return(
+      <div className="heroIntro">
+        <h1>Personal <span>Date Night</span></h1>
+        <p>Sometimes you just need a break from your partner but you already got a babysitter. Now couples can go to different events in similar locations and at the same time.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut aliquam obcaecati totam quasi est quia in sapiente animi illo dicta.</p>
+      </div>
+    )
+  }
 }
 
 export default Intro;
