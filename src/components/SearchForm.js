8160721eaff25e81a1b3c720ce5db09c5b13d457
Irene Truong <irenesis123@gmail.com>
2019-06-11 16:15:37 -0400

    header styles down to 500-600px size

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -66,7 +66,7 @@ class SearchForm extends Component{
             <div>
               <div>
                 <div className="heroFormBlockTime">
-                  <div>
+                  <div className="perTimeContainer">
                     <label htmlFor="time">From</label>
                     <TimePicker
                       format="h:mm a"
@@ -77,7 +77,7 @@ class SearchForm extends Component{
                       required
                     />
                   </div>
-                  <div>
+                  <div className="perTimeContainer">
                     <label htmlFor="time">Until</label>
                     <TimePicker
                       format="h:mm a"
