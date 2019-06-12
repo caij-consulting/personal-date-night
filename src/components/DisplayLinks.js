be1017fe0d665fe14188c12d88d235e1ca9d415a
anniebombanie <anniebombanie@gmail.com>
2019-06-11 20:27:12 -0400

    fix modal overlay bug

--- src/components/DisplayLinks.js
+++ src/components/DisplayLinks.js
@@ -28,11 +28,11 @@ loopFocus= () => {
       </button>
         <div>{this.props.children}</div>
 
-        <h2>Congratulations! You've both successfully chosen your events!</h2>
+        <h2><span>Congratulations!</span> You've both successfully chosen your events!</h2>
         <p>You can buy your tickets here:</p>
-        <p>User One: <a href={this.props.user1choice.url} target="_blank" rel="noopener noreferrer" aria-label="Click to get your tickets here!">{this.props.user1choice.name}</a> </p>
+        <FontAwesomeIcon icon="user" aria-hidden="true" /><p>User One: <a href={this.props.user1choice.url} target="_blank" rel="noopener noreferrer" aria-label="Click to get your tickets here!">{this.props.user1choice.name}</a> </p>
       
-        <p>User Two: <a href={this.props.user2choice.url} target="_blank" rel="noopener noreferrer" aria-label="Click to get your tickets here!">{this.props.user2choice.name}</a></p>
+        <FontAwesomeIcon icon="user" aria-hidden="true" /><p>User Two: <a href={this.props.user2choice.url} target="_blank" rel="noopener noreferrer" aria-label="Click to get your tickets here!">{this.props.user2choice.name}</a></p>
         <a href="#" className="keepFocusModal" onTransitionEnd={this.loopFocus} aria-hidden="true"></a>
       </div>
     );
